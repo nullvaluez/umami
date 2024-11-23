cb04d487b721f2b5dabb1f6eaa118d138593703d
nullvaluez <118377972+nullvaluez@users.noreply.github.com>
2024-11-23 14:47:09 -0500

    Update check-db.js

--- scripts/check-db.js
+++ scripts/check-db.js
@@ -10,7 +10,7 @@ if (process.env.SKIP_DB_CHECK) {
   process.exit(0);
 }
 
-function getDatabaseType(url = process.env.DATABASE_URL) {
+function getDatabaseType(url = process.env.DATABASE_URL && process.env.DIRECT_DATABASE_URL) {
   const type = url && url.split(':')[0];
 
   if (type === 'postgres') {
@@ -31,10 +31,10 @@ function error(msg) {
 }
 
 async function checkEnv() {
-  if (!process.env.DATABASE_URL) {
-    throw new Error('DATABASE_URL is not defined.');
+  if (!process.env.DATABASE_URL && !process.env.DIRECT_DATABASE_URL) {
+    throw new Error('DATABASE_URL and DIRECT is not defined.');
   } else {
-    success('DATABASE_URL is defined.');
+    success('DATABASE_URL and DIRECT is defined.');
   }
 }
 
